99b6310226b715800ed243543c89bcc7b33742d0
sivaturi9 <sivaturi@vlcybernetics.com>
2024-03-08 14:48:27 -0500

    changes

---
+++ app/forms/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+class TimerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    time = IntegerField('Time', validators=[DataRequired()])
+    global_timer = BooleanField('IsGlobal',validators=[DataRequired()])
